48eef94862ab9bc8b2ca5d1e2423889dee3c4dad
nizar <nizarrr.id@gmail.com>
2025-09-01 23:28:45 +0700

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import React from "react"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+   return (
+      <div>
+         <Link
+            href="/"
+            className="text-2xl font-bold flex gap-2 items-center ">
+            <Image
+               src="/image/logo.png"
+               alt="Logo"
+               width={50}
+               height={50}
+            />
+            Visuaally
+         </Link>
+      </div>
+   )
 }
 
 export default Navbar
